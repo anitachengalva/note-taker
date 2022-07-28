c5676beb8dc90ac99e4abf3ce546165d4e7febf8
Anita Chengalva <anita.chengalva@gmail.com>
2022-07-28 02:28:47 -0700

    updated html routes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const express = require("express");
 const fs = require('fs');
+const uuid = require('uuid');
+const notes = require('./db/db.json');
 
 const PORT = process.env.PORT || 3001;
 
@@ -10,22 +12,29 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
 
 // ROUTES -------------------------------------------------
 // API Routes ---------------------------------------------
-// * `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
+// GET /api/notes
+app.get('/notes')
+// should read the `db.json` file and return all saved notes as JSON.
 
-// * `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+// POST /api/notes
+// should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 
-// `DELETE /api/notes/:id` should receive a query parameter that contains the id of a note to delete. To delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+// DELETE /api/notes/:id
+// should receive a query parameter that contains the id of a note to delete. To delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
 
 // HTML Routes ---------------------------------------------
-// * `GET /notes` should return the `notes.html` file.
-// * `GET *` should return the `index.html` file.
+// GET /notes
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// GET 
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
