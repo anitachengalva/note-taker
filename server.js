0a766828ab31493e85c79a8968905c916885f47b
Anita Chengalva <anita.chengalva@gmail.com>
2022-07-28 02:10:24 -0700

    updating requirements for api and html routes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const express = require("express");
-const fs = require('fs')
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3001;
 
@@ -17,3 +17,15 @@ app.get('/', (req, res) =>
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
+
+// ROUTES -------------------------------------------------
+// API Routes ---------------------------------------------
+// * `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
+
+// * `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+
+// `DELETE /api/notes/:id` should receive a query parameter that contains the id of a note to delete. To delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+
+// HTML Routes ---------------------------------------------
+// * `GET /notes` should return the `notes.html` file.
+// * `GET *` should return the `index.html` file.
